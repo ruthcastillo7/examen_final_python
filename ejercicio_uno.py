7700090032401d3e6e6c7473a565bd6b3a113b1c
ruthcastillo7 <castillohuamaniruthm@gmail.com>
2024-07-17 10:30:08 -0500

    ejercicio:uno

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,19 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+
+lista={3,6,5,7,8,4,1}
+
+def lista_num(lista:list):
+    mayor=lista[0]
+    for n in lista:
+        if n > mayor:
+            mayor=n
+    return mayor
+def lista_num(lista:list):
+    menor=lista[0]
+    for x in lista:
+        if x < menor:
+            menor=x
+    return menor
+print(f"{lista}")
