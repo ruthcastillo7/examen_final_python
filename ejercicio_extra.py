ef6845d6844241f108407909f857a3814e8050ce
ruthcastillo7 <castillohuamaniruthm@gmail.com>
2024-07-17 10:57:59 -0500

    ejercicio:extra

--- ejercicio_extra.py
+++ ejercicio_extra.py
@@ -5,3 +5,31 @@ y crear las siguientes funciones para el procesamiento de su informacion
 2. funcion que me permita actualizar el horario de atencion.
 3. funcion que me permita eliminar una tienda comercial.
 """
+lista_alumnos=[
+    {
+        "tienda":"bara",
+        "celular":928453777,
+    },
+     {
+        "tienda":"mavelita",
+        "celular":928453788,
+    },
+     {
+        "tienda":"baras",
+        "celular":928452772,
+    },
+     {
+        "tienda":"nina",
+        "celular":928456666,
+    },
+     {
+        "tienda":"luffy",
+        "celular":928454444,
+    }
+]
+def objeto(e):
+        e["tienda"]="zoro"
+        return [e]
+
+lista_actualizada=list(filter(objeto,lista_alumnos))
+print(lista_actualizada[1])
